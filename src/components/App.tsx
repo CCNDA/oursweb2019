214232676606d446857512dc1c4171a4cece2718
Hsin-lin Cheng aka lancetw <lancetw@gmail.com>
2019-04-07 00:08:09 +0800

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export interface AppProps {}
+
+export const App = (props: AppProps) => <h1>Oursweb 2019</h1>
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { hydrate, render } from 'react-dom'
+
+import { App } from './components/App'
+
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
